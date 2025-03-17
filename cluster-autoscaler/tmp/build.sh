141bb1dccb47b3e958b08c659c797720720386d9
Dmitry Sergeev <identw@gmail.com>
2025-03-17 19:35:30 +0400

    identw-hetzner build hacks

--- cluster-autoscaler/tmp/build.sh
+++ cluster-autoscaler/tmp/build.sh
@@ -3,7 +3,9 @@ set -xe
 
 version=$1
 
-gofmt cloudprovider/hetzner/*.go 1>/dev/null
+gofmt cloudprovider/hetzner-identw/*.go 1>/dev/null
+rm -fv ./cluster-autoscaler-arm64
+rm -fv ./cluster-autoscaler-amd64
 make docker-builder
 make build-in-docker
 docker build -t docker.io/identw/cluster-autoscaler:${version} -f tmp/Dockerfile .
